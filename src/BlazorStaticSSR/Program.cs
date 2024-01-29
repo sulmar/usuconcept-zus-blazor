c9b8ecd895734d0a8963c30611a810737ba35317
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-01-29 12:17:44 +0100

    Dodanie interaktywności - tryb po stronie serwera

--- src/BlazorStaticSSR/Program.cs
+++ src/BlazorStaticSSR/Program.cs
@@ -4,7 +4,8 @@ using Microsoft.AspNetCore.Http.HttpResults;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -21,7 +22,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.MapGet("/greeting", () => new RazorComponentResult<GreetingComponent>());
 
