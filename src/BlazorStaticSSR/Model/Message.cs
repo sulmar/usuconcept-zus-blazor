8dcdfe05ed1ecb79b90d4dd6c7b2dd4fb852da05
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-01-29 12:00:37 +0100

    Dodanie walidacji

--- src/BlazorStaticSSR/Model/Message.cs
+++ src/BlazorStaticSSR/Model/Message.cs
@@ -1,7 +1,11 @@
-﻿namespace BlazorStaticSSR.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorStaticSSR.Model;
 
 public class Message
 {
+    [Required, MinLength(3), MaxLength(20)]
     public string Subject { get; set; }
+    [Required]
     public string Content { get; set; }
 }
