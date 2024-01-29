7367acf19e8ea3c46e8d1418c7000c9b369e5c0c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-01-29 15:48:30 +0100

    Zastosowanie partial class

---
+++ src/BlazorSSRSakilaApp/Components/Pages/Film/List.razor.cs
@@ -0,0 +1,19 @@
+﻿using BlazorSSRSakilaApp.Infrastructure;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSSRSakilaApp.Components.Pages.Film;
+
+[StreamRendering]
+public partial class List
+{
+    private List<Model.Film> films;
+
+    [Inject]
+    public SakilaContext context { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        films = await context.Films.ToListAsync();
+    }
+}
