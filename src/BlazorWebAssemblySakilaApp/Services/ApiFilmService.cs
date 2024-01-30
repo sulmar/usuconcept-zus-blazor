defa95c7aee5424ced86944589e286764f662cde
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-01-30 10:15:58 +0100

    Fix

--- src/BlazorWebAssemblySakilaApp/Services/ApiFilmService.cs
+++ src/BlazorWebAssemblySakilaApp/Services/ApiFilmService.cs
@@ -3,7 +3,7 @@ using System.Net.Http.Json;
 
 namespace BlazorWebAssemblySakilaApp.Services;
 
-public class ApiFilmService : IFilmService
+public class ApiFilmService
 {
     private readonly HttpClient Http;
 
