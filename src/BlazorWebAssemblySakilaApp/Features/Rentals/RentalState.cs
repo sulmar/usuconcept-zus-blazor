07e91479e90e8fd3fddc16bc9a9b07ef0959f2d0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-02-02 13:48:26 +0100

    Ładowanie danych za pomocą Fluxor

---
+++ src/BlazorWebAssemblySakilaApp/Features/Rentals/RentalState.cs
@@ -0,0 +1,47 @@
+﻿using BlazorWebAssemblySakilaApp.Services;
+using Fluxor;
+using Sakila.Model;
+
+namespace BlazorWebAssemblySakilaApp.Features.Rentals;
+
+public record RentalsState(bool IsLoading, List<Rental> Rentals);
+
+
+public class RentalsFeature : Feature<RentalsState>
+{
+    public override string GetName() => "Rentals";
+    protected override RentalsState GetInitialState() => new(false, Enumerable.Empty<Rental>().ToList());
+}
+
+public record FetchRentalsAction;
+public record FetchRentalsResultAction(List<Rental> Rentals);
+
+public class Effects
+{
+    private readonly ApiRentalService Api;
+
+    public Effects(ApiRentalService api)
+    {
+        Api = api;
+    }
+
+    [EffectMethod]
+    public async Task HandleFetchRentalAction(FetchRentalsAction action, IDispatcher dispatcher)
+    {
+        var rentals = await Api.GetAllAsync();
+
+        if (rentals is not null)
+        {
+            dispatcher.Dispatch(new FetchRentalsResultAction(rentals));
+        }
+    }
+}
+
+public static class Reducers
+{
+    [ReducerMethod]
+    public static RentalsState OnFetchRentalsAction(RentalsState state, FetchRentalsAction action) => new RentalsState(true, null);
+   
+    [ReducerMethod]
+    public static RentalsState OnFetchRentalsResultAction(RentalsState state, FetchRentalsResultAction action) => new RentalsState(false, action.Rentals);
+}
